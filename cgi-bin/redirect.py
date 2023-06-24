89bb356eb37767aa9d03e9d0dbd66d0e2e518b8a
pumapumapumas <pumapumapumas@gmail.com>
2023-06-24 16:26:20 -0400

    Added Services page and revised Learning Library

---
+++ cgi-bin/redirect.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import http.server
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/default.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+if __name__ == '__main__':
+    http.server.test(HandlerClass=MyHTTPRequestHandler)
