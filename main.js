a940a9d669c7852fe297f5e6937bd7af75851c4e
pumapumapumas <pumapumapumas@gmail.com>
2023-06-26 13:56:06 -0400

    mobile site menu on default page

---
+++ main.js
@@ -0,0 +1,16 @@
+// main.js
+
+// Get the necessary DOM elements
+var menuToggle = document.querySelector('#menu-toggle');
+var mobileAside = document.querySelector('.mobile-aside');
+
+mobileAside.classList.add('show');
+
+// Add an event listener to the menu toggle checkbox
+menuToggle.addEventListener('change', function() {
+  if (this.checked) {
+    mobileAside.classList.add('show');
+  } else {
+    mobileAside.classList.remove('show');
+  }
+});
